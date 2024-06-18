a64930c7eb41b38ae5f104aa842f2e87f185c238
Jean Carlos <santosjeancarlos70@gmail.com>
2024-06-17 23:43:28 -0300

    Nova Atualizacao

--- src/main/java/br/com/alura/scheenmatch/ScheenmatchApplication.java
+++ src/main/java/br/com/alura/scheenmatch/ScheenmatchApplication.java
@@ -15,14 +15,5 @@ public class ScheenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.exibeMenu();
-
-	//		List<DadosTemporadas> temporadas = new ArrayList<>();
-	//		for (int i = 1 ; i <= dados.totalTemporada(); i++){
-	//			json = consumoAPI.obterDados("http://www.omdbapi.com/?t=Impuros&season="+i+"&apikey=3ee820f8");
-	//			DadosTemporadas dadosTemporadas = converteDados.obterDados(json, DadosTemporadas.class);
-	//			temporadas.add(dadosTemporadas);
-	//		}
-	//
-	//		temporadas.forEach(System.out::println);
 	}
 }
--- src/main/java/br/com/alura/scheenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/scheenmatch/principal/Principal.java
@@ -1,9 +1,13 @@
 package br.com.alura.scheenmatch.principal;
 
+import br.com.alura.scheenmatch.model.DadosEpisodio;
 import br.com.alura.scheenmatch.model.DadosSerie;
+import br.com.alura.scheenmatch.model.DadosTemporadas;
 import br.com.alura.scheenmatch.service.ConsumoAPI;
 import br.com.alura.scheenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -21,5 +25,24 @@ public class Principal {
 
         DadosSerie dados = converteDados.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+
+        List<DadosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1 ; i <= dados.totalTemporada(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ","+")+"&season="+i+ API_KEY);
+            DadosTemporadas dadosTemporadas = converteDados.obterDados(json, DadosTemporadas.class);
+            temporadas.add(dadosTemporadas);
+        }
+
+        temporadas.forEach(System.out::println);
+
+        /*for (int i = 0; i < dados.totalTemporada(); i++) {
+            List<DadosEpisodio> episodiosTemtorada = temporadas.get(i).episodios();
+            System.out.println("Teste" +i);
+            for (int j = 0; j < episodiosTemtorada.size(); j++) {
+                System.out.println(episodiosTemtorada.get(j).titulo());
+            }
+        }*/
+
+        temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
     }
 }
